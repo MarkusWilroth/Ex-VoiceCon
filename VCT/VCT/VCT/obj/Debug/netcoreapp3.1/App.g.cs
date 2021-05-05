69bccb5b12d93457e162191257798dcb751a21b9
Markus Wilroth <37120955+MarkusWilroth@users.noreply.github.com>
2021-05-05 16:07:49 +0200

    MnuError fix

--- VCT/VCT/VCT/obj/Debug/netcoreapp3.1/App.g.cs
+++ VCT/VCT/VCT/obj/Debug/netcoreapp3.1/App.g.cs
@@ -45,7 +45,7 @@ namespace VCT {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace VCT {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
         public static void Main() {
             VCT.App app = new VCT.App();
             app.InitializeComponent();
